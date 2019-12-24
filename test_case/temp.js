fd2ca4de0c8721cac8be235458a5fc6a2930640d
krisxu <kris.xu@assaabloy.com>
2019-12-24 17:34:16 +0800

    # optimize: add temp.js

---
+++ test_case/temp.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "register": [
+        {
+            "api_name": "test1",
+            "method_type": "post",
+            "url": "localhost://users",
+            "response": {
+                "result": "success"
+            },
+            "body": {
+                "productId": 69767671,
+                "productName": "fugiat cupidatat",
+                "price": -69614691.75144641
+            }
+        },
+        {
+            "api_name": "test2",
+            "method_type": "get",
+            "url": "localhost://users",
+            "query": {
+                "productId": 69767671,
+                "productName": "dolor consectetur occaecat pariatur do",
+                "price": -43374497.19882414
+            }
+        }
+    ]
+};
