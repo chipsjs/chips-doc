634f3a967eeaa8c079e0b1f6ef758a4397a3f7f2
krisxu <kris.xu@assaabloy.com>
2020-11-03 12:06:06 +0800

    fix(convert/tag): fix bug about auto generate error tag

--- test/convert/tag_spec.js
+++ test/convert/tag_spec.js
@@ -128,4 +128,33 @@ describe('convert spec to auto generate tag', () => {
       fs.unlinkSync('test/convert/temp/tag.json');
     });
   });
+
+  describe('api name like A/:id/:id', () => {
+    before('set source data', () => {
+      specJson = {
+        'Get /test/:id/:id': {
+          method: 'post',
+          response: {
+            body: {
+            }
+          }
+        }
+      }
+    });
+
+    before('convert normal spec to api doc', () => {
+      specResult = Convert.getInstance().run(specJson, 'test/convert/temp/tag');
+    });
+
+    it('should generate correct api doc', () => {
+      const api_name = '/test/:id/:id';
+      assert.exists(specResult[api_name]);
+      assert.exists(specResult[api_name].get);
+      assert.nestedPropertyVal(specResult[api_name], 'get.tags[0]', 'test');
+    });
+
+    after('clean file', () => {
+      fs.unlinkSync('test/convert/temp/tag.json');
+    });
+  });
 });
