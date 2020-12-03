d8d9268ccdf4fffb15515647c8a7ee5709c19411
krisxu <kris.xu@assaabloy.com>
2020-12-03 13:48:09 +0800

    fix(convert/tag): parse tag error when tag is A/:id/B/:id

--- test/convert/tag_spec.js
+++ test/convert/tag_spec.js
@@ -157,4 +157,33 @@ describe('convert spec to auto generate tag', () => {
       fs.unlinkSync('test/convert/temp/tag.json');
     });
   });
+
+  describe('api name like A/:id/B/:id', () => {
+    before('set source data', () => {
+      specJson = {
+        'Get /A/:id/B/:id': {
+          method: 'get',
+          response: {
+            body: {
+            }
+          }
+        }
+      }
+    });
+
+    before('convert normal spec to api doc', () => {
+      specResult = Convert.getInstance().run(specJson, 'test/convert/temp/tag');
+    });
+
+    it('should generate correct api doc', () => {
+      const api_name = '/A/:id/B/:id';
+      assert.exists(specResult[api_name]);
+      assert.exists(specResult[api_name].get);
+      assert.nestedPropertyVal(specResult[api_name], 'get.tags[0]', 'A');
+    });
+
+    after('clean file', () => {
+      fs.unlinkSync('test/convert/temp/tag.json');
+    });
+  });
 });
